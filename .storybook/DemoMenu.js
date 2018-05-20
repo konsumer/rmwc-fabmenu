0ed0378e16ac5a234df8b55f3f6238cef859a722
David Konsumer <konsumer@jetboystudio.com>
2018-05-19 17:09:02 -0700

    better action gif, better centering with width/height 100%

--- .storybook/DemoMenu.js
+++ .storybook/DemoMenu.js
@@ -3,7 +3,7 @@ import { action } from '@storybook/addon-actions'
 import { Fab } from 'rmwc/Fab'
 import icons from './icons.json'
 
-const randomIcons = (new Array(5).fill()).map(() => icons[(Math.random() * icons.length) | 0])
+const randomIcons = (new Array(3).fill()).map(() => icons[(Math.random() * icons.length) | 0])
 const DemoMenu = (props) => randomIcons.map((icon, i) => <Fab {...props} onClick={action(icon)} key={i}>{icon}</Fab>)
 
 export default DemoMenu
